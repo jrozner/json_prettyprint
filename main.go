d7d687232e5d91551de295cc475d51f158adc42b
Joe Rozner <joe@deadbytes.net>
2013-12-01 14:19:01 -0800

    Initial commit

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+func main() {
+	var unmarshaled interface{}
+
+	if len(os.Args) != 2 {
+		printUsage()
+	}
+
+	rawData, err := ioutil.ReadFile(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = json.Unmarshal(rawData, &unmarshaled)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	marshaled, err := json.MarshalIndent(unmarshaled, "", " ")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(marshaled))
+}
+
+func printUsage() {
+	fmt.Printf("Usage: %s <file>\n", os.Args[0])
+	os.Exit(1)
+}
